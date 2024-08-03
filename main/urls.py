8f59770505c27878bc97fd76d021b95d3b936e45
TioCaminhao <savio.v@escolar.ifrn;edu.br>
2024-08-03 16:52:48 -0300

    sa

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pds3.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ariel/', ariel, name='ariel'),
+    path('', savio, name="savio")
 ]
--- pds3/views.py
+++ pds3/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ariel(request):
+    return render(request, 'ariel.html')
+
+def savio(request):
+    return render(request, 'savio.html')
